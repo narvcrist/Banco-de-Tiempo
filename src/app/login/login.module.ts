e738547ea8dd720d3f6a343839992b63e71f0a2b
Casa <ram-mds@hotmail.com>
2019-02-06 00:43:15 -0500

    Mañana luciraste

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpModule
     
   ]
 })
